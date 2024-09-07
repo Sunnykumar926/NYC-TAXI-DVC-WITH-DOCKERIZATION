81ed4d4169f2357018d2cbabf54ee66f4fcac8bb
Sunnykumar926 <118978144+Sunnykumar926@users.noreply.github.com>
2024-09-07 15:44:16 +0530

    Added cookie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A machine learning project using docker and data version control',
+    author='sunny',
+    license='MIT',
+)
